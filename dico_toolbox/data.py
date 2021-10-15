45bfcde46f5d589d723dcabc1bddf29ea4664cd8
Marco Pascucci <marpas.paris@gmail.com>
2021-10-15 15:36:33 +0200

    add pclean file provider to data

--- dico_toolbox/data.py
+++ dico_toolbox/data.py
@@ -1,5 +1,6 @@
 from glob import glob
-import os, re
+import os
+import re
 import logging
 from difflib import get_close_matches
 import functools
@@ -10,42 +11,48 @@ try:
     from soma import aims as _aims
     HAS_AIMS = True
 except ImportError:
-    HAS_AIMS = False    
+    HAS_AIMS = False
+
 
 def _with_brainvisa(fun):
     @functools.wraps(fun)
-    def wrapper(*args,**kwargs):
-        if not HAS_AIMS: raise RuntimeError("This function is only available in a brainvisa environment")
-        return fun(*args,**kwargs)
+    def wrapper(*args, **kwargs):
+        if not HAS_AIMS:
+            raise RuntimeError(
+                "This function is only available in a brainvisa environment")
+        return fun(*args, **kwargs)
     return wrapper
 
 
 class paths:
     dico = "/neurospin/dico"
 
-    
+
 class learnclean:
     """file provider for the folder /neurospin/lnao/PClean/database_learnclean"""
 
     base = "/neurospin/lnao/PClean/database_learnclean/all"
     subfolders = glob(f"{base}/*/")
     arg_path_suffix = "t1mri/t1/default_analysis/folds/3.3/base2018_manual/"
-    
+
     # the absolute paths of the arg files
     # eg /neurospin/lnao/PClean/database_learnclean/all/sujet01/t1mri/t1/default_analysis/folds/3.3/base2018_manual/Rsujet01_base2018_manual.arg
     graph_paths = list()
     for subfolder in subfolders:
         graph_paths += glob(f"{subfolder}{arg_path_suffix}*.arg")
-    
+
     # the absolute paths of all left arg files
-    left_paths = list(filter(lambda s: os.path.basename(s).startswith('L'), graph_paths))
-    
+    left_paths = list(
+        filter(lambda s: os.path.basename(s).startswith('L'), graph_paths))
+
     # the absolute paths of all right arg files
-    right_paths = list(filter(lambda s: os.path.basename(s).startswith('L'), graph_paths))
-    
+    right_paths = list(
+        filter(lambda s: os.path.basename(s).startswith('L'), graph_paths))
+
     # file names of all args (withour lmeading L or R)
-    names = list(set(map(lambda s : os.path.basename(s).split('.')[0][1:], graph_paths)))
-    
+    names = list(
+        set(map(lambda s: os.path.basename(s).split('.')[0][1:], graph_paths)))
+
     @classmethod
     def get_graph_path_by_name(cls, name, side):
         """Get the absolute path of an arg file by file basename and side (L or R)"""
@@ -56,18 +63,71 @@ class learnclean:
             m = get_close_matches(name, cls.names)
             s = f" Did you mean '{m[0]}' ?" if m else ''
             raise ValueError(f"'{name}' is not a valid valid name.{s}")
-                      
+
         r = re.compile(f".*?{side.upper()}({name}).arg")
-        return list(filter(r.match, cls.graph_paths))[0] 
+        return list(filter(r.match, cls.graph_paths))[0]
 
     @classmethod
     @_with_brainvisa
     def get_graph_by_name(cls, name, side):
         """Get the aims graph by file basename and side (L or R)"""
         path = cls.get_graph_path_by_name(name, side)
-        graph = _aims.read(path)        
+        graph = _aims.read(path)
         return graph
-    
+
+    @classmethod
+    @_with_brainvisa
+    def get_left_graph_by_name(cls, name):
+        """Get the LEFT aims graph by file basename"""
+        return cls.get_graph_by_name(name, 'L')
+
+    @classmethod
+    @_with_brainvisa
+    def get_right_graph_by_name(cls, name):
+        """Get the RIGHT aims graph by file basename"""
+        return cls.get_graph_by_name(name, 'R')
+
+
+class pclean:
+    """file provider for the plcean folder"""
+
+    base = "/neurospin/dico/data/bv_databases/human/pclean/all"
+    subfolders = glob(f"{base}/*/")
+    arg_path_suffix = "t1mri/t1/default_analysis/folds/3.3/base2018_manual/"
+    skeleton_path_suffix = "t1mri/t1/default_analysis/segmentation/"
+
+    # the absolute paths of the arg files
+    # eg /neurospin/lnao/PClean/database_learnclean/all/sujet01/t1mri/t1/default_analysis/folds/3.3/base2018_manual/Rsujet01_base2018_manual.arg
+    graph_paths = list()
+    for subfolder in subfolders:
+        graph_paths += glob(f"{subfolder}{arg_path_suffix}*.arg")
+
+    # file names of all args (withour lmeading L or R)
+    names = list(
+        set(map(lambda s: os.path.basename(s).split('.')[0][1:], graph_paths)))
+
+    @classmethod
+    def get_graph_path_by_name(cls, name, side):
+        """Get the absolute path of an arg file by file basename and side(L or R)"""
+        assert isinstance(name, str)
+        assert side.upper() in "LR", "Side must be either 'L' or 'R'"
+
+        if not name in learnclean.names:
+            m = get_close_matches(name, cls.names)
+            s = f" Did you mean '{m[0]}' ?" if m else ''
+            raise ValueError(f"'{name}' is not a valid valid name.{s}")
+
+        r = re.compile(f".*?{side.upper()}({name}).arg")
+        return list(filter(r.match, cls.graph_paths))[0]
+
+    @classmethod
+    @_with_brainvisa
+    def get_graph_by_name(cls, name, side):
+        """Get the aims graph by file basename and side(L or R)"""
+        path = cls.get_graph_path_by_name(name, side)
+        graph = _aims.read(path)
+        return graph
+
     @classmethod
     @_with_brainvisa
     def get_left_graph_by_name(cls, name):
