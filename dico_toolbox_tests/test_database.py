ac9a13d76f9ba85554238b9740d57346f01281f9
bc264254 <bastiencagna@gmai.com>
2021-11-02 10:16:43 +0100

    Remove usage  of unittest

--- dico_toolbox_tests/test_database.py
+++ dico_toolbox_tests/test_database.py
@@ -1,35 +1,27 @@
 
-import unittest
 from dico_toolbox.database import create_test_database, extend_templates
 
 
-class TestDatabaseUtils(unittest.TestCase):
+def test_extend_templates(self):
+    fpaths = extend_templates(
+        "[center]/[subject]/t1mri/[acq]/[ana]/folds/[version]/[session]/[hemi][subject]_[session].arg",
+        default_value="*",
+        subject="sub-01", session="session_manual", hemi=["L", "R"])
 
-    def test_extend_templates(self):
-        fpaths = extend_templates(
-            "[center]/[subject]/t1mri/[acq]/[ana]/folds/[version]/[session]/[hemi][subject]_[session].arg",
-            default_value="*",
-            subject="sub-01", session="session_manual", hemi=["L", "R"])
-
-        assert len(fpaths) == 2
-        assert fpaths[0] == "*/sub-01/t1mri/*/*/folds/*/session_manual/Lsub-01_session_manual.arg"
-        assert fpaths[1] == "*/sub-01/t1mri/*/*/folds/*/session_manual/Rsub-01_session_manual.arg"
+    assert len(fpaths) == 2
+    assert fpaths[0] == "*/sub-01/t1mri/*/*/folds/*/session_manual/Lsub-01_session_manual.arg"
+    assert fpaths[1] == "*/sub-01/t1mri/*/*/folds/*/session_manual/Rsub-01_session_manual.arg"
 
 
-class TestDatabaseMethods(unittest.TestCase):
-
-    def test_database(self):
-        db = create_test_database()
-        assert len(db.list_all('subject')) == 2
-        assert len(db.get(subject='001')) == 29
-
-    def test_database_with_templates(self):
-        db = create_test_database()
-        fpaths = db.get_from_template(
-            "morphologist_labelled_graph",
-            subject=['001'], version='3.3', session="session1_manual", hemi=["L", "R"])
-        assert len(fpaths) == 2
+def test_database(self):
+    db = create_test_database()
+    assert len(db.list_all('subject')) == 2
+    assert len(db.get(subject='001')) == 29
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_database_with_templates(self):
+    db = create_test_database()
+    fpaths = db.get_from_template(
+        "morphologist_labelled_graph",
+        subject=['001'], version='3.3', session="session1_manual", hemi=["L", "R"])
+    assert len(fpaths) == 2
--- dico_toolbox_tests/test_graph.py
+++ dico_toolbox_tests/test_graph.py
@@ -1,12 +1,6 @@
-import unittest
 import dico_toolbox as dtb
 
 
-class TestGraph(unittest.TestCase):
-    def test_list_buckets():
-        graph_path = dtb.database.create_test_database().get(type="graph")[0]
-        buckets = dtb.graph.list_buckets(graph_path, transform="Talairach")
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_list_buckets():
+    graph_path = dtb.database.create_test_database().get(type="graph")[0]
+    buckets = dtb.graph.list_buckets(graph_path, transform="Talairach")
