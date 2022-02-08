7c04e8107168050519c39c7eff57cb9999a54909
Denis Rivière <denisri@users.noreply.github.com>
2022-02-08 10:51:18 +0100

    use an info.py file to allow integration with casa-distro/bv_maker (#12)
    this is just a separation of what was in setup.py into 2 files, the
    info.py file being read by bv_maker not only for setup. Moreover it
    allows to get info at runtime (to get library version etc) when needed.

--- setup.py
+++ setup.py
@@ -6,17 +6,6 @@ import logging as _logging
 _log = _logging.getLogger(__name__)
 
 
-def get_property(prop, project):
-    lookup_file = '/__init__.py'
-    result = re.findall(r'^{}\s*=\s*[\'"]([^\'"]*)[\'"]$'.format(
-        prop), open(project + lookup_file).read(), re.MULTILINE)
-    # re.findall always returns list
-    if len(result) != 1:
-        _log.warn("Alert: More than one occurrence of {} found in {}}".format([prop, project + lookup_file]))
-    assert type(result[0]) == str
-    return result[0]
-
-
 try:
     from aims import soma
 except:
@@ -26,26 +15,24 @@ except:
 with open(op.join(op.split(__file__)[0], "README.md"), "r") as fh:
     long_description = fh.read()
 
+release_info = {}
+python_dir = os.path.dirname(__file__)
+with open(os.path.join(python_dir, "dico_toolbox", "info.py")) as f:
+    code = f.read()
+    exec(code, release_info)
+
 
 setuptools.setup(
-    name='dico_toolbox',
-    version=get_property('__version__', 'dico_toolbox'),
-    description="A common toolbox to the FoldDico project",
-    author='Marco Pascucci, Bastien Cagna',
+    name=release_info['NAME'],
+    version=release_info['__version__'],
+    description=release_info['DESCRIPTION'],
+    author=release_info['AUTHOR'],
+    author_email=release_info['AUTHOR_EMAIL']
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author_email='marpas.paris@gmail.com',
-    url='',
+    url=release_info['URL'],
     packages=['dico_toolbox'],
-    install_requires=['numpy'],
-    extras_require={
-        'dev': [
-            'pytest'
-        ]
-    },
-    classifiers=[
-        "Programming Language :: Python",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-        "Operating System :: OS Independent",
-    ]
+    install_requires=release_info["REQUIRES"],
+    classifiers=release_info["CLASSIFIERS"]
+    extras_require=release_info['EXTRA_REQUIRES']
 )
